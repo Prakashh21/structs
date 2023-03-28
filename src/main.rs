db8ce30b85006ce564ef534f121a9c7d0a303950
Prakash <prakashope@gmail.com>
2023-03-28 18:14:48 +0000

    structs in rust

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in_count: u64,
+    }
+
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    print!("{0}", user1.email)
+}
